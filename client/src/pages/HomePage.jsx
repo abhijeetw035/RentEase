5dedb2bd17ed59b142c08601e6da945b67990066
abhijeetw035 <abhijeetw035@gmail.com>
2024-07-20 00:26:51 +0530

    Slides & Categories

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navabar from '../components/Navabar';
+import Slide from '../components/Slide';
+import Categories from '../components/Categories';
 
 const HomePage = () => {
   return (
     <div>
       {" "}
       <Navabar />
+      <Slide />
+      <Categories />
     </div>
   );
 }
